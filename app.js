eaf2850413c630a8a94122f14069b91722387517
amsully <asulliva@umass.edu>
2017-09-25 22:47:56 -0400

    learn emit (from client and server)

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	console.log('A user connected');
 
+	// Send message after timeout of 4 seconds
+	setTimeout(function(){
+		// socket.send('Sent a message 4 seconds after connection');
+		socket.emit('testerEvent', {description: 'A custom event named testerEvent'} );
+	}, 4000);
+
+	socket.on('clientEvent', function(data) {
+		console.log(data);
+	});
+
 	// Whenever somebody disconnects
 	socket.on('disconnect', function() {
 		console.log('A user disconnected');
