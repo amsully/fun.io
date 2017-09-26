ace275fbd67faea66258155046feecfdd4c1c4ad
amsully <asulliva@umass.edu>
2017-09-25 23:02:24 -0400

    broadcast to specific users

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ app.get('/', function(req, res){
 	res.sendFile( __dirname + '/index.html');
 });
 
+var clients = 0
+
 io.on('connection', function(socket){
 	console.log('A user connected');
+	clients++;
 
-	// Send message after timeout of 4 seconds
-	setTimeout(function(){
-		// socket.send('Sent a message 4 seconds after connection');
-		socket.emit('testerEvent', {description: 'A custom event named testerEvent'} );
-	}, 4000);
+	socket.emit('newClientConnect', {description: "Hey!"})
+	socket.broadcast.emit('newClientConnect', {description:clients + ' clients connected'});
+	// io.sockets.emit('broadcast', {description: clients + ' clients connected!'})
 
-	socket.on('clientEvent', function(data) {
-		console.log(data);
-	});
-
-	// Whenever somebody disconnects
-	socket.on('disconnect', function() {
-		console.log('A user disconnected');
+	socket.on('disconnect', function(){
+		clients--;
+		io.sockets.emit('broadcast', {description: clients + 'clients connected!'});
 	});
 });
 
